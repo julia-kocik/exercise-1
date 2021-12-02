18038c744da7bcf00028d38364a55d4f0ec47182
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-12-02 16:45:01 +0100

    Add adjustment in script.js sort method

--- docs/js/script.js
+++ docs/js/script.js
@@ -7,8 +7,8 @@ const generateNumbers = () => {
   for(let i = 0; i < 20; i++) {
     allNumbers.push(Math.floor(Math.random()*100 +1));
   }
-  const evenNumbers = allNumbers.filter(item => item%2 === 0).sort();
-  const oddNumbers = allNumbers.filter(item => item %2 !== 0).sort();
+  const evenNumbers = allNumbers.filter(item => item%2 === 0).sort((a,b) => a-b);
+  const oddNumbers = allNumbers.filter(item => item %2 !== 0).sort((a,b) => a-b);
   even.innerHTML = evenNumbers.join(', ');
   odd.innerHTML = oddNumbers.join(', ');
 };
