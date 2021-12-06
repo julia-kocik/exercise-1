bed1f3d4b064fd46dd23f270fb4b29fdb30f41ec
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-12-06 14:27:05 +0100

    Add adjustments in script to not repeat numbers

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,21 @@ const button = document.querySelector('.button');
 const even = document.querySelector('.even');
 const odd = document.querySelector('.odd');
 
+const checkNumber = (num, arr) => {
+  if (arr.includes(num)) {
+    return num + 1;
+  } else if (arr.includes(num) && num === 100) {
+    return num - 1;
+  } else {
+    return num;
+  }
+};
+
 const generateNumbers = () => {
   const allNumbers = [];
   for(let i = 0; i < 20; i++) {
-    allNumbers.push(Math.floor(Math.random()*100 +1));
+    let random = checkNumber(Math.floor(Math.random()*100 +1), allNumbers);
+    allNumbers.push(random);
   }
   const evenNumbers = allNumbers.filter(item => item%2 === 0).sort((a,b) => a-b);
   const oddNumbers = allNumbers.filter(item => item %2 !== 0).sort((a,b) => a-b);
