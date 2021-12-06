8577dcbb8f24432ff2e9e5eb9e09bbde5da37658
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-12-06 20:47:29 +0100

    Fix the bug in generateNumbers

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,15 +2,11 @@ const button = document.querySelector('.button');
 const even = document.querySelector('.even');
 const odd = document.querySelector('.odd');
 
-const checkNumber = (num, arr) => {
-  return arr.includes(num) ? num + 1 : arr.includes(num) && num === 100 ? num - 1 : num;
-};
-
 const generateNumbers = () => {
   const allNumbers = [];
-  for(let i = 0; i < 20; i++) {
-    let random = checkNumber(Math.floor(Math.random()*100 +1), allNumbers);
-    allNumbers.push(random);
+  while (allNumbers.length < 20) {
+    let random = Math.floor(Math.random()*100 +1);
+    !allNumbers.includes(random) && allNumbers.push(random);
   }
   const evenNumbers = allNumbers.filter(item => item%2 === 0).sort((a,b) => a-b);
   const oddNumbers = allNumbers.filter(item => item %2 !== 0).sort((a,b) => a-b);
